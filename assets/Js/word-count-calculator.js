5d6711a805a847e1db13f9443c25c749f1404408
devanshtivari <devanshtiwari80771@gmail.com>
2021-10-01 03:07:47 +0530

    Added functionality to word count

--- assets/Js/word-count-calculator.js
+++ assets/Js/word-count-calculator.js
@@ -1,11 +1,32 @@
-var print = function(msg) {
-
-  document.getElementById("output").innerHTML = "Length is " + msg;
-
+function count_word( val ){
+  var wom = val.match(/\S+/g);
+  return {
+      words : wom ? wom.length : 0
+  };
 }
-
-document.getElementById("btn").onclick = function(event){
-
-    print(document.getElementById('str').value.length);
+function count_character(val){
+  return val.length;
 }
-
+function count_alphabets(val){
+  let remText = val.replace(/ /g, "")
+  let l = remText.length;
+  return l;
+}
+var textContent = document.getElementById("textcontent");
+var showWordCount   = document.getElementById("countWord");
+var showcharactercount = document.getElementById("countcharacter");
+var showalphabetcount = document.getElementById("countalphabets");
+textContent.addEventListener("input", function(){
+var v = count_word( this.value );
+showWordCount.innerHTML = (
+    "<br>Words: "+ v.words
+);
+var x = count_character(this.value);
+showcharactercount.innerHTML = (
+  "<br>Characters: "+x
+);
+var d = count_alphabets(this.value);
+showalphabetcount.innerHTML = (
+  "<br>Alphabets: "+d
+);
+}, false);
