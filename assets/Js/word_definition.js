faba127f7ab4203452055ab42c4188ab02eddc84
Sachin Mittal <sachinmittal24687@gmail.com>
2022-03-15 16:11:03 +0530

    Modified word definition webpage

--- assets/Js/word_definition.js
+++ assets/Js/word_definition.js
@@ -1,78 +1,57 @@
-let input = document.querySelector("#input");
+const container = document.querySelector(".container"),
+  searchInput = container.querySelector("input"),
+  sound = document.getElementById("sound");
+(infoText = container.querySelector(".info-text")),
+  (removeIcon = container.querySelector(".search span"));
 
-let searchBtn = document.querySelector("#search");
-
-let apiKey = "0d5bd3f3-7fa8-4993-9018-5ff058fbb89b";
-
-let notFound = document.querySelector(".not_found");
-
-let defBox = document.querySelector(".def");
-
-let audioBox = document.querySelector(".audio");
-
-let loading = document.querySelector(".loading");
-
-searchBtn.addEventListener('click', function(e){
-    e.preventDefault();
-
-    audioBox.innerHTML = '';
-    notFound.innerText = '';
-    defBox.innerText = '';
-
-    let word = input.value
-
-    if(word === ''){
-        alert('Word is required');
-        return;
-    }
-
-    getData(word);
-})
-
-async function getData(word){
-    loading.style.display = 'block';
-    const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`);
-    const data = await response.json();
-
-    if(!data.length){
-        loading.style.display = 'none';
-        notFound.innerText = 'No result found';
-        return;
-    }
-
-    if(typeof data[0] === 'string'){
-        loading.style.display = 'none';
-        let heading = document.createElement('h3');
-        heading.innerText = 'Did you mean?'
-        notFound.appendChild(heading);
-        data.forEach(element => {
-            let suggestion = document.createElement('span');
-            suggestion.classList.add('suggested');
-            suggestion.innerText = element;
-            notFound.appendChild(suggestion);
-
-        })
-        return;
-    }
-
-    loading.style.display = 'none';
-    let definition = data[0].shortdef[0];
-    defBox.innerText = definition;
-
-    const soundName = data[0].hwi.prs[0].sound.audio;
-    if(soundName){
-        renderSound(soundName);
-    }
-    console.log(data);
+function data(result, word) {
+  if (result.title) {
+    infoText.innerHTML = `Oops ;) we can't find <span>"${word}"</span>.`;
+  } else {
+    container.classList.add("active");
+    let definitions = result[0].meanings[0].definitions[0];
+    phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
+    document.querySelector(".word p").innerText = result[0].word;
+    document.querySelector(".word span").innerText = phontetics;
+    document.querySelector(".meaning span").innerText = definitions.definition;
+    sound.setAttribute("src", `https:${result[0].phonetics[1].audio}`);
+  }
 }
 
-function renderSound(soundName){
-    // https://media.merriam-webster.com/soundc11
-    let subfolder = soundName.charAt(0);
-    let soundSrc = `https://media.merriam-webster.com/soundc11/${subfolder}/${soundName}.wav?key=${apiKey}`;
-
-    let aud = document.createElement('audio');
-    aud.src = soundSrc;
-    aud.controls = true;
-    audioBox.appendChild(aud);
+function playSound() {
+  sound.play();
+  console.log("hello");
 }
+
+function search(word) {
+  fetchApi(word);
+  searchInput.value = word;
+}
+
+function fetchApi(word) {
+  container.classList.remove("active");
+  infoText.innerHTML = `Searching :) `;
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => data(result, word))
+    .catch(() => {
+      infoText.innerHTML = `Oops ;) we can't find <span>"${word}"</span>.`;
+    });
+}
+
+searchInput.addEventListener("keyup", (e) => {
+  let word = e.target.value.replace(/\s+/g, " ");
+  if (e.key == "Enter" && word) {
+    fetchApi(word);
+  }
+});
+
+removeIcon.addEventListener("click", () => {
+  searchInput.value = "";
+  searchInput.focus();
+  container.classList.remove("active");
+  infoText.style.color = "#9A9A9A";
+  infoText.innerHTML =
+    "Type any existing word and press enter to get meaning, example, synonyms, etc.";
+});
