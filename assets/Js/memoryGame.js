dae9c9dc293cb8c2ee9c30ae7f50fb4036ef3a77
Soumali <gorai.soumali@gmail.com>
2022-04-14 11:11:40 +0530

    Changed the file location

--- assets/Js/memoryGame.js
+++ assets/Js/memoryGame.js
@@ -5,69 +5,66 @@ var scoreText = document.querySelectorAll(".score")[0];
 var imageSource = [
   {
     id: 0,
-    source: "../../assets/Images/memoryGame/burger.png",
-    usedCount: 0
+    source: "../assets/Images/memoryGame/burger.png",
+    usedCount: 0,
   },
   {
     id: 1,
-    source: "../../assets/Images/memoryGame/donut.png",
-    usedCount: 0
+    source: "../assets/Images/memoryGame/donut.png",
+    usedCount: 0,
   },
   {
     id: 2,
-    source: "../../assets/Images/memoryGame/fried-chicken.png",
-    usedCount: 0
+    source: "../assets/Images/memoryGame/fried-chicken.png",
+    usedCount: 0,
   },
   {
     id: 3,
-    source: "../../assets/Images/memoryGame/ice-cream.png",
-    usedCount: 0
+    source: "../assets/Images/memoryGame/ice-cream.png",
+    usedCount: 0,
   },
   {
     id: 4,
-    source: "../../assets/Images/memoryGame/milkshake.png",
-    usedCount: 0
+    source: "../assets/Images/memoryGame/milkshake.png",
+    usedCount: 0,
   },
   {
     id: 5,
     source: "../../assets/Images/memoryGame/muffin.png",
-    usedCount: 0
+    usedCount: 0,
   },
   {
     id: 6,
-    source: "../../assets/Images/memoryGame/noodles.png",
-    usedCount: 0
+    source: "../assets/Images/memoryGame/noodles.png",
+    usedCount: 0,
   },
   {
     id: 7,
-    source: "../../assets/Images/memoryGame/pizza.png",
-    usedCount: 0
-  }
-]
+    source: "../assets/Images/memoryGame/pizza.png",
+    usedCount: 0,
+  },
+];
 
 var img = document.querySelectorAll("img");
 var playAgain = document.querySelector(".restart");
 
-window.addEventListener('DOMContentLoaded' , function() {
-  for(var i = 0; i < img.length; i++) {
+window.addEventListener("DOMContentLoaded", function () {
+  for (var i = 0; i < img.length; i++) {
     img[i].style.zIndex = "-1";
     img[i].src = generateImageSource();
   }
 });
 
-
 function generateImageSource() {
-
   var randomNumber = Math.floor(Math.random() * 10000) % 8;
 
-  while(imageSource[randomNumber].usedCount == 2) {
+  while (imageSource[randomNumber].usedCount == 2) {
     randomNumber = Math.floor(Math.random() * 10000) % 8;
   }
 
   imageSource[randomNumber].usedCount++;
 
   return imageSource[randomNumber].source;
-
 }
 
 var prevBtn = "";
@@ -75,19 +72,17 @@ var curBtn = "";
 
 var totalMoves = 0;
 var successfulMoves = 0;
- 
-for(var i = 0; i < button.length; i++) {
-  button[i].addEventListener("click" , function () {
 
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function () {
     totalMoves++;
-    var img = this.querySelector("img")
-    if(img.style.zIndex === "-1") {
+    var img = this.querySelector("img");
+    if (img.style.zIndex === "-1") {
       img.style.zIndex = "2";
       this.disabled = true;
       this.classList.remove("btn-hover");
 
-
-      if(prevBtn === "") {
+      if (prevBtn === "") {
         prevBtn = this;
       } else {
         curBtn = this;
@@ -95,16 +90,15 @@ for(var i = 0; i < button.length; i++) {
         var curImage = curBtn.querySelector("img").src;
         var prevImage = prevBtn.querySelector("img").src;
 
-        if(curImage == prevImage) {
+        if (curImage == prevImage) {
           prevBtn.disabled = true;
           prevBtn.classList.remove("btn-hover");
           curBtn.disabled = true;
           curBtn.classList.remove("btn-hover");
 
-
           var temp = [];
-          for(var i = 0; i < button.length; i++) {
-            if(button[i] !== curBtn && button[i] !== prevBtn) {
+          for (var i = 0; i < button.length; i++) {
+            if (button[i] !== curBtn && button[i] !== prevBtn) {
               temp.push(button[i]);
             }
           }
@@ -118,24 +112,23 @@ for(var i = 0; i < button.length; i++) {
           var scores = Math.ceil((successfulMoves / totalMoves) * 100);
           score.textContent = scores;
 
-          if(successfulMoves == 16) {
-
-            var audio = new Audio("../../assets/Audio/memoryGame/game-over.wav");
+          if (successfulMoves == 16) {
+            var audio = new Audio("../assets/Audio/memoryGame/game-over.wav");
             audio.play();
             scores = Math.ceil((successfulMoves / totalMoves) * 100);
-            scoreText.textContent = "Final Score :"
+            scoreText.textContent = "Final Score :";
           } else {
-            var audio = new Audio("../../assets/Audio/memoryGame/correct.wav");
+            var audio = new Audio("../assets/Audio/memoryGame/correct.wav");
             audio.play();
           }
         } else {
-          var audio = new Audio("../../assets/Audio/memoryGame/wrong.wav");
+          var audio = new Audio("../assets/Audio/memoryGame/wrong.wav");
           audio.play();
-          for(var i = 0; i < button.length; i++) {
-              button[i].disabled = true;
-              button[i].classList.remove("btn-hover");
+          for (var i = 0; i < button.length; i++) {
+            button[i].disabled = true;
+            button[i].classList.remove("btn-hover");
           }
-          setTimeout(function() {
+          setTimeout(function () {
             curBtn.querySelector("img").style.zIndex = -1;
             prevBtn.querySelector("img").style.zIndex = -1;
             curBtn.disabled = false;
@@ -144,47 +137,37 @@ for(var i = 0; i < button.length; i++) {
             prevBtn.classList.add("btn-hover");
             prevBtn = "";
             curBtn = "";
-          } , 1000);
-          setTimeout(function() {
-            for(var i = 0; i < button.length; i++) {
-                button[i].disabled = false;
-                button[i].classList.add("btn-hover");
+          }, 1000);
+          setTimeout(function () {
+            for (var i = 0; i < button.length; i++) {
+              button[i].disabled = false;
+              button[i].classList.add("btn-hover");
             }
-          } , 1100);
+          }, 1100);
         }
-
       }
-    } 
+    }
   });
 }
 
-
-playAgain.addEventListener('click' , function() {
+playAgain.addEventListener("click", function () {
   totalMoves = 0;
   successfulMoves = 0;
   prevBtn = "";
   curBtn = "";
-  for(var i = 0; i < buttons.length; i++) {
+  for (var i = 0; i < buttons.length; i++) {
     buttons[i].disabled = false;
     buttons[i].classList.add("btn-hover");
     buttons[i].querySelector("img").style.zIndex = -1;
   }
   button = buttons;
-  for(var i = 0; i < imageSource.length; i++) {
+  for (var i = 0; i < imageSource.length; i++) {
     imageSource[i].usedCount = 0;
   }
-  for(var i = 0; i < img.length; i++) {
+  for (var i = 0; i < img.length; i++) {
     img[i].style.zIndex = "-1";
     img[i].src = generateImageSource();
   }
   score.textContent = "0";
   scoreText.textContent = "Live Score";
 });
-
-
-
-
-
-
-
-
