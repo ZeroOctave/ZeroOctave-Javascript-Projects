d14bffb10b68634cac638da80b1e807411abf73c
iamrahulmahato <rahul25mahato@gmail.com>
2022-04-19 06:38:23 -0700

    :memo: js variable added

---
+++ assets/Js/pokemon.js
@@ -0,0 +1,22 @@
+var errors = 0;
+var cardList = [
+    "darkness",
+    "double",
+    "fairy",
+    "fighting",
+    "fire",
+    "grass",
+    "lightning",
+    "metal",
+    "psychic",
+    "water"
+]
+
+
+var cardSet;
+var board = [];
+var rows = 4;
+var columns = 5;
+
+var card1Selected;
+var card2Selected;
