3c05e8f791654fc90272e2da4dafc20c76f08ef1
Monarch <monarchk7@gmail.com>
2022-05-01 12:21:23 +0530

    updated reflex game

--- assets/Js/Reflex_training.js
+++ assets/Js/Reflex_training.js
@@ -1,13 +1,6 @@
 var startTime = new Date();
 var endTime = new Date();
-var startPressed = false;
-var bgChangeStarted = false;
-var maxWait = 20;
 var timerID;
-var randMULTIPLIER = 0x015a4e35;
-var randINCREMENT = 1;
-var today = new Date();
-var randSeed = today.getSeconds();
 
 function getRandomcoordinate(max) {
   return Math.floor(Math.random() * max);
@@ -21,17 +14,12 @@ function setRandomPos() {
   document.getElementById("target").style.display = "inherit";
 }
 function viewNull() {
-  // console.log(document.getElementById("demo").id);
-  // document.body.style.background = "white";
   document.getElementById("container").style.display = "none";
 }
 
 function viewShowSecond() {
-  if ((document.getElementById("playground").tagName = "DIV")) {
-    console.log(document.getElementById("playground").tagName);
-  }
-  // var elementType = ;
-  // console.log(elementType);
+  document.getElementById("page").style.display = "inherit";
+
   document.getElementById("playground").style.display = "inherit";
 }
 
@@ -40,81 +28,59 @@ function viewShowFirst() {
   document.getElementById("playground").style.display = "none";
 }
 
-// function viewShowFirst() {
-//   document.getElementById("container").style.display = "inherit";
-// }
-
 function testStart() {
+  startTime = new Date();
   viewNull();
   viewShowSecond();
-  // setRandomPos();
-  timerID = setTimeout("setRandomPos()", 4000);
-  // document.getElementById("result").textContent = "Test Started";
-  //   if (startPressed) {
-  //     alert("Already started. Press stop to stop");
-  //     return;
-  //   } else {
-  //     startPressed = true;
-  //     timerID = setTimeout("startTest()", 6000 * randNumber());
-  //   }
+  timerID = setTimeout("setRandomPos()", 5000);
 }
 function remark(responseTime) {
   var responseString = "";
-  if (responseTime < 0.3) {
+  if (responseTime < 0.6) {
     responseString = "Dang you defeated Levi!";
   }
-  if (responseTime >= 0.3) {
+  if (responseTime >= 0.6) {
     responseString = "Noice :)";
   }
-  if (responseTime >= 0.4) {
-    responseString = "Eh...Try harder";
-  }
-
-  if (responseTime >= 0.5) {
+  if (responseTime >= 0.8) {
     responseString = "Keep practicing!";
   }
 
-  if (responseTime >= 0.6) {
+  if (responseTime >= 1) {
+    responseString = "Eh...Try harder";
+  }
+
+  if (responseTime >= 1.5) {
     responseString = "lol what was that are you okay?";
   }
 
-  if (responseTime >= 1) {
+  if (responseTime >= 2.5) {
     responseString = "You sleeping or what?";
   }
 
   return responseString;
 }
 
-function timeTaken() {
-  console.log(responseTime);
-}
-
 function testStop() {
+  clearTimeout(timerID);
   viewShowFirst();
   document.getElementById("target").style.display = "none";
-  // if (bgChangeStarted) {
-  //   endTime = new Date();
+  document.getElementById("result").style.display = "none";
+}
 
-  //   document.body.style.background = "white";
-  //   console.log(responseTime);
-  //   console.log(document.getElementById("result").textContent);
-  //   document.getElementById("result").textContent =
-  //     "Your response time is " +
-  //     responseTime +
-  //     "," +
-  //     " " +
-  //     remark(responseTime);
-  //   startPressed = false;
-  //   bgChangeStarted = false;
-  // } else {
-  //   if (!startPressed) {
-  //     // e;
-  //     document.getElementById("result").textContent = "Press Start first";
-  //   } else {
-  //     clearTimeout(timerID);
-  //     startPressed = false;
-  //     document.getElementById("result").textContent =
-  //       "You clicked too soon :( Try Again";
-  //   }
-  // }
+function timeTaken() {
+  endTime = new Date();
+  var responseTime = (endTime.getTime() - startTime.getTime()) / 1000;
+  console.log(responseTime - 5);
+  var finalTime = responseTime.toFixed(3) - 5.0;
+  clearTimeout(timerID);
+  document.getElementById("result").style.display = "inherit";
+  document.getElementById("result").textContent =
+    "Your response time is " +
+    finalTime.toFixed(3) +
+    "," +
+    " " +
+    remark(finalTime.toFixed(3));
+  document.getElementById("target").style.display = "none";
+  document.getElementById("page").style.display = "none";
 }
