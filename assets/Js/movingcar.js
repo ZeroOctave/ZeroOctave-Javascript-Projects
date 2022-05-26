1477134d39637212dc76723f0e507e0ed5e381c1
Mr. Ånand <73425223+Astrodevil@users.noreply.github.com>
2022-05-26 13:56:15 +0530

    Update movingcar.js

--- assets/Js/movingcar.js
+++ assets/Js/movingcar.js
@@ -1,4 +1,4 @@
 var audio = document.createElement('audio');
-audio.setAttribute('src', 'assets/Audio/sound.mp3'); 
+audio.setAttribute('src', '../assets/Audio/sound.mp3'); 
 audio.loop=true;
 audio.play();
