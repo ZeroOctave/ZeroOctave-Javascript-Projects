823c84e2994c1e6de68f0eff0b72ed932f87680c
sumeetk10 <sumeet.k2022@gmail.com>
2022-09-15 20:13:05 +0530

    Revert "updated ReportCalculator"
    This reverts commit 9e6925f291d7604bec8e6f8d6b43d582a8af6249.

--- assets/Js/ReportCalculator.js
+++ assets/Js/ReportCalculator.js
@@ -1,15 +1,14 @@
 document.getElementById("button").addEventListener("click", total);
 
 function total() {
-  let w = document.getElementById("chem").value;
+  let w = document.getElementById("webd").value;
 
   let x = document.getElementById("physics").value;
   let y = document.getElementById("maths").value;
-  let z = document.getElementById("eng").value;
-  let v = document.getElementById("bio").value;
+  let z = document.getElementById("abcd").value;
   var totalgrade =
-    parseFloat(w) + parseFloat(x) + parseFloat(y) + parseFloat(z) + parseFloat(v);
-  var percentage = (totalgrade / 500) * 100;
+    parseFloat(w) + parseFloat(x) + parseFloat(y) + parseFloat(z);
+  var percentage = (totalgrade / 400) * 100;
 
   var grade = "";
   if (percentage <= 100 && percentage >= 90) {
