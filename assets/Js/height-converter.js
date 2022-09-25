c78e94c7f7e584491f193f4eeea4bb8eee13c207
Drishti Jain <100796147+Drishti-jain21@users.noreply.github.com>
2022-09-25 14:13:54 +0530

    Changed the UI of Height Convertor

--- assets/Js/height-converter.js
+++ assets/Js/height-converter.js
@@ -1,17 +1,18 @@
-
-//BMI = KG / (H/100 * H/100)
-
-document.getElementById("submit").addEventListener("click", heightConverter);
-
- function heightConverter(){
- 	var feet = parseInt(document.getElementById("feet").value *12);
- 	var inches = parseInt(document.getElementById("inches").value);
-
- 	var cm = (feet + inches) *2.54;
-	var n = cm.toFixed(0);
-
-
-
- 	document.getElementById("result").innerHTML = n;
-
- }
+document.getElementById("clickme").addEventListener("click",()=>{    
+	const a=document.querySelector(".feet").value;
+	const b=document.querySelector(".inches").value;
+	const output=document.querySelector(".output");
+	 const d=a*30.48+b*2.54;
+	 console.log(d);
+	 output.textContent="Converted Height is "+ d+"cm";
+	document.querySelector(".output").style.color= "white";
+	});
+  
+	document.getElementById("reset").addEventListener("click",()=>{  
+	let a=document.querySelector(".feet");
+	let b=document.querySelector(".inches");
+	let output=document.querySelector(".output");
+	  a.value="";
+	  b.value="";
+	output.textContent= " ";
+  });
