8eda8227a87277052ae51df1899762f32b6787aa
Manmohan Dhal <96486531+manmohan4142@users.noreply.github.com>
2022-10-04 09:12:19 +0530

    Added Javascript Project

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-jQuery(document).ready(function() {
-    jQuery('.toggle').click(function() {
-        jQuery('body').toggleClass('day');
-        jQuery('body').toggleClass('night');
-    });
-});
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+    if(this.scroll > 500){
+       $('.scroll-up-btn').addClass("show");
+    }else{
+       $('.scroll-up-btn').removeClass("show");
+    }
+  });
+  //slide-up script
+     $('.scroll-up-btn').click(function(){
+       $('html').animate({scrollTop: 0});
+     });
 
-const nav = document.querySelector(".nav");
 
-let projects = [];
-const experi = document.getElementById('cardd');
-const searchBar = document.getElementById("searchbar");
 
-searchBar.addEventListener('keyup', (e) => {
-  const searchString = e.target.value;
-  const filteredProjects = projects.filter( projects => {
-      
-      return projects.name.toLowerCase().includes(searchString.toLowerCase());
+  //toggle menu/navbar script
+  $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
   });
 
-  console.log(filteredProjects)
-  displayProjects(filteredProjects);
+
+  //typing animation script
+    var typed = new Typed(".typing", {
+      strings: ["Youtuber","Developer","Blogger","Designer","Freelancer"],
+      typeSpeed: 100,
+      backSpeed: 60;
+      loop: true
+    });
+    var typed = new Typed(".typing-2", {
+      strings: ["Youtuber","Developer","Blogger","Designer","Freelancer"],
+      typeSpeed: 100,
+      backSpeed: 60;
+      loop: true
+    });
+
+
+
+ //  owl carousel script
+   $('.carousel').owlCarousel({
+       margin: 20,
+       loop: true;
+       autoplayTimeOut: 1000,
+       autoplayHoverPause: true,
+       responsive:{
+          0:{
+            items: 1,
+            nav: false
+          },
+          600:{
+            items: 2,
+            nav: false
+          },
+          1000:{
+            items: 3,
+            nav: false
+          }
+       }
+   });
 });
-
-const getProject = fetch('cards.json')
-                .then(response => response.json())
-                .then(data => {
-                    projects = data;
-                    displayProjects(projects)
-                    console.log(projects)
-
-});
-
-const displayProjects = (projects) => {
-  const htmlString = projects
-.map((project) => {
-  return`<div class="stylebox">
-  <p class="card-heading">${project.name}</p>
-       <div class="image" >
-           <img src="${project.image}" alt="">
-        </div>
-        <div class="card-data">
-            <a href=${project.link} target="_blank"><button class="btnn">View</button></a>
-        </div>
- 
-    </div>`;
-    
-})
-
-experi.innerHTML = htmlString;
-
-}
-
-getProject();
