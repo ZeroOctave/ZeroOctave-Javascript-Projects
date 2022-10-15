876544d4dc74c879556fe009d3dadc1a712a1a44
Harsh Mishra <78163963+harshmishra414@users.noreply.github.com>
2022-10-15 15:37:36 +0530

    Add files via upload

---
+++ Public/Word Guessing Game/js/word.js
@@ -0,0 +1,126 @@
+const words = [
+  {
+    word: 'atomic',
+    type: 'adjective',
+    meaning: 'relating to an atom or atoms',
+    synonyms: ['small', 'micro', 'tiny']
+  },
+  {
+    word: 'hijack',
+    type: 'verb',
+    meaning: 'unlawfully seize (an aircraft) in transit and force it to go to a different destination for one own purposes.',
+    synonyms: ['seize', 'commandeer', 'take over']
+  },
+  {
+    word: 'jumble',
+    type: 'verb',
+    meaning: 'mix up in a confused or untidy way',
+    synonyms: ['mix up', 'muddle up', 'disarrange']
+  },
+  {
+    word: 'quench',
+    type: 'verb',
+    meaning: `satisfy (one's thirst) by drinking.`,
+    synonyms: ['put out', 'smother', 'extinguish']
+  },
+  {
+    word: 'sample',
+    type: 'noun',
+    meaning: 'a small part or quantity intended to show what the whole is like.',
+    synonyms: ['specimen', 'representative', 'trial']
+  },
+  {
+    word: 'frenzy',
+    type: 'noun',
+    meaning: 'a state or period of uncontrolled excitement or wild behavior.',
+    synonyms: ['hysteria', 'madness', 'wildness']
+  },
+  {
+    word: 'object',
+    type: 'noun',
+    meaning: 'a material thing that can be seen and touched.',
+    synonyms: ['thing', 'item', 'piece']
+  },
+  {
+    word: 'acquire',
+    type: 'verb',
+    meaning: 'to obtain an asset or object for oneself.',
+    synonyms: ['possess', 'take in', 'capture']
+  },
+  {
+    word: 'chariot',
+    type: 'noun',
+    meaning: 'a two-wheeled horse-drawn vehicle used in ancient warfare and racing.',
+    synonyms: ['carraige', 'wheeled cart', 'horse-drawn vehicle']
+  },
+  {
+    word: 'garnish',
+    type: 'verb',
+    meaning: 'a two-wheeled horse-drawn vehicle used in ancient warfare and racing.',
+    synonyms: ['decoration', 'adornment', 'embellish']
+  },
+  {
+    word: 'country',
+    type: 'noun',
+    meaning: 'a nation with its own government, occupying a particular territory.',
+    synonyms: ['state', 'nation', 'kingdom']
+  },
+  {
+    word: 'picture',
+    type: 'noun',
+    meaning: 'represent (someone or something) in a photograph or picture',
+    synonyms: ['sketch', 'print', 'canvas']
+  },
+  {
+    word: 'mailbox',
+    type: 'noun',
+    meaning: 'a private box into which mail is delivered.',
+    synonyms: ['post box', 'pillar box', 'letter drop']
+  },
+  {
+    word: 'mediator',
+    type: 'noun',
+    meaning: 'a person who attempts to make people involved in a conflict come to an agreement.',
+    synonyms: ['arbiter', 'middleman', 'moderator']
+  },
+  {
+    word: 'sandwich',
+    type: 'noun',
+    meaning: 'an item of food consisting of two pieces of bread with meat, cheese, or other filling between them, eaten as a light meal.',
+    synonyms: ['bread', 'burger', 'tunaburger']
+  },
+  {
+    word: 'computer',
+    type: 'noun',
+    meaning: 'an electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program.',
+    synonyms: ['number cruncher', 'computing machine', 'imformation processor']
+  },
+  {
+    word: 'outbreak',
+    type: 'noun',
+    meaning: 'the sudden or violent start of something unwelcome, such as war, disease.',
+    synonyms: ['outburst', 'upsurge', 'eruption']
+  },
+  {
+    word: 'oculars',
+    type: 'adjective',
+    meaning: 'of or connected with the eyes or vision.',
+    synonyms: ['optical', 'visual', 'seeable']
+  },
+  {
+    word: 'repolish',
+    type: 'verb',
+    meaning: 'to polish something again',
+    synonyms: ['reshine', 'glaze', 'luster']
+  },
+  {
+    word: 'donuts',
+    type: 'noun',
+    meaning: 'a small fried cake of sweetened dough, typically in the shape of a ball or ring.',
+    synonyms: ['sweet roll', 'bun', 'dunker']
+  },
+  
+]
+
+
+const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
