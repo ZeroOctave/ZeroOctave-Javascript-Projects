464fef9aace98a6f76c38958aec9767a64b581e7
surbhi400 <77582783+surbhi400@users.noreply.github.com>
2023-04-29 11:08:23 +0530

    updating Email Validator

--- assets/Js/EmailValidator.js
+++ assets/Js/EmailValidator.js
@@ -1,25 +1,21 @@
 let nameinform = document.getElementById('email');
 let button = document.getElementById('submit');
-
-function submit()
-{
-    if(nameinform.value.includes("@") && nameinform.value.includes("."))
-    {
-        if(nameinform.value.indexOf("@")>0 && nameinform.value.indexOf(".")>nameinform.value.indexOf("@") && nameinform.value.indexOf(".")!=nameinform.value.length)
-        {
-            alert("Your Email Id is Valid");
-        }
-        else
-        {
-            alert("Please Provide Correct Email Address");
-        }
-    }
-    else
-    {
-        alert("Please Provide Correct Email Address");
-    }
-
+function ValidateEmail(inputText) {
+  var data = inputText.value;
+    var atSign = data.indexOf("@");
+    var dotSign = data.indexOf(".");
+  if (atSign<1 ) {
+    alert("Invalid @ position");
+    return false;
+  }
+  else if (dotSign<atSign+2 || dotSign+2>=data.length){
+     alert("Invalid . position");
+    return false;
+  }
+  else{
+    alert("Your Email Id is correct");
+    return true;
+  }
 }
-button.addEventListener('click',submit);
 
 
