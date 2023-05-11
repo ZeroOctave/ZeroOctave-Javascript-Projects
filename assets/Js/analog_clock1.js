122b1570f48066be7ac67802ff7f766fc9d82de3
lasya <lakshmilasya.kata@gmail.com>
2023-05-12 00:09:19 +0530

    added requested changes

--- assets/Js/analog_clock1.js
+++ assets/Js/analog_clock1.js
@@ -18,15 +18,18 @@ const checkbox = document.getElementById("checkbox");
 const body = document.querySelector("body");
 const label = document.querySelector(".label");
 const root = document.querySelector(":root");
-checkbox.addEventListener("change", function() {
-    if (this.checked) {
-        body.style.background = "black";
-        label.style.backgroundColor = "#40DCA5";
-        root.style.setProperty('--sec-color', '#fff');
-    } else {
-        body.style.background = "skyblue";
-        label.style.backgroundColor = "white";
-        root.style.setProperty('--sec-color', 'red');
-
-    }
+const ball = document.querySelector(".ball");
+checkbox.addEventListener("change", function () {
+if (this.checked) {
+    body.style.background = "black";
+    label.style.backgroundColor = "#40DCA5";
+    root.style.setProperty("--sec-color", "#fff");
+    ball.style.backgroundColor = "#fff";
+} else {
+    body.style.background = "skyblue";
+    label.style.backgroundColor = "white";
+    root.style.setProperty("--sec-color", "red");
+    ball.style.backgroundColor = "black";
+}
 });
+                
