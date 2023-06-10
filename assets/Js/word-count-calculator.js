4b9dafc2aedcfdef773a382aadc8641d399ec06f
Aswini Samantray <aswinisamantray.7149@gmail.com>
2023-06-10 21:57:36 +0530

    Added the feature to know the frequency of a word in text.

--- assets/Js/word-count-calculator.js
+++ assets/Js/word-count-calculator.js
@@ -25,14 +25,37 @@ function longest(val){
     },"");
     return longestWord;
 }
-// var temp=document.getElementById("textcontent");
-temp=document.getElementById("textcontent");
+
+function search(val,search){
+  let sentence=val.split(" ");
+  let count=0;
+  if(search=='') 
+    return 0;
+  for(let i=0;i<sentence.length;i++){
+    if(sentence[i]==search && sentence[sentence.length-1]!=" ") {
+      count=count+1;
+    }
+  }
+  return count;
+ }
+
+ function copy(val){
+  return val;
+}
+ 
+// temp=document.getElementById("textcontent");
 var textContent = document.getElementById("textcontent");
 var showWordCount   = document.getElementById("countWord");
 var showcharactercount = document.getElementById("countcharacter");
 var showalphabetcount = document.getElementById("countalphabets");
 var longestWord=document.getElementById("longestWord");
 var longestWordlength=document.getElementById("longestWordlength");
+var searchText=document.getElementById("searchText");//Word to be searched
+var frequency=document.getElementById("frequency");
+var copy=document.getElementById("copyButton");
+var paste=document.getElementById("pasteButton");
+let f=0;
+longestWord.innerHTML=("<br>There is no text.");
 textContent.addEventListener("input", function(){
   
 var v = count_word( this.value );
@@ -48,6 +71,21 @@ showalphabetcount.innerHTML = (
   "<br>Alphabets: "+d
 );
 var l=longest(this.value);
-longestWord.innerHTML=("<br>Longest Word: "+l);
+if(l.length===0){
+  longestWord.innerHTML=("<br>There is no text.");
+}
+else{
+  longestWord.innerHTML=("<br>Longest Word: "+l.bold());
+}
+
 longestWordlength.innerHTML=("<br>Length of "+l+" is: "+l.length);
+let text=this.value;
+  searchText.addEventListener("input",function(){
+    let word=this.value;
+    f=search(text,word);
+    searchButton.addEventListener("click",function(){
+      frequency.innerHTML=("<br>"+word.bold()+" occurs "+f+" times(s) in the text.");
+    },false);
+  },false);
 }, false);
+
