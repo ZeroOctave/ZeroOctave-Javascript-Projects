295cc918c8ae4f6a034091148cd66492fab39009
Mozibx <opuzmoses@gmail.com>
2023-08-23 00:48:55 +0100

    chores: updated homepage js file

---
+++ index2.js
@@ -0,0 +1,46 @@
+const toogleBtn = document.querySelector(".toogle_btn");
+const toogleBtnIcon = document.querySelector(".toogle_btn i");
+const menu = document.querySelector(".dropdown_menu");
+const menuLists = document.querySelectorAll(".menu_lists li");
+const navScroll = document.querySelector(".navbar");
+
+/* NAV-FUNCTIONS */
+
+/* ACTIVE LINKS */
+const removeActive = () => {
+  menuLists.forEach((link) => {
+    link.classList.remove("active");
+  });
+};
+
+menuLists.forEach((link) => {
+  link.addEventListener("click", () => {
+    removeActive();
+    link.classList.add("active");
+  });
+});
+
+toogleBtn.onclick = () => {
+  menu.classList.toggle("open");
+
+  const isOpen = menu.classList.contains("open");
+
+  toogleBtnIcon.classList = isOpen ? "fa fa-times" : "fa fa-bars";
+};
+
+const isClose = () => {
+  const isClose = menu.classList.remove("open");
+
+  toogleBtnIcon.classList = isClose ? "fa fa-times" : "fa fa-bars";
+};
+
+document.onclick = (e) => {
+  if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
+    isClose();
+  }
+};
+document.onscroll = (e) => {
+  if (!toogleBtn.contains(e.target) && !menu.contains(e.target)) {
+    isClose();
+  }
+};
